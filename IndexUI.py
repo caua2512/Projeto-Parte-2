2e10ad7b8d5a09667e5a187faab496378050f6cd
caua2512 <130388387+caua2512@users.noreply.github.com>
2023-12-21 18:17:07 -0300

    Update IndexUI.py

--- IndexUI.py
+++ IndexUI.py
@@ -10,24 +10,30 @@ from Templates.SacarUI import SacarUI
 from Templates.DepositarUI import DepositarUI
 from Templates.TransferirUI import TransferirUI
 from Templates.Listar_Transferencias_RecentesUI import ListarTransferenciasUI
+from View import view
 import streamlit as st
 
 class IndexUI:
-
-  def menu_temporario():
-    op = st.sidebar.selectbox("Menu", ["Login","Abrir Conta Sistema","Editar Perfil","Abrir Conta","Manter Banco", "manter Cliente","Manter Contas","Depositar","Sacar","Transferir","Listar Transferencias"])
+  def menu_visitante():
+    op = st.sidebar.selectbox("Menu", ["Login", "Abrir Conta Sistema"])
+    if op == "Login": loginUI.main()
+    if op == "Abrir Conta Sistema": AbrirContaSistemaUI.main()
+  def menu_admin():
+    op = st.sidebar.selectbox("Menu", ["Manter Banco", "manter Cliente","Manter Contas","Editar Perfil"])
     if op == "Manter Banco": ManterBancoUI.main()
     if op == "manter Cliente": ManterClienteUI.main()
     if op == "Manter Contas": ManterContaUI.main()
-    if op == "Login": loginUI.main()
-    if op == "Abrir Conta Sistema": AbrirContaSistemaUI.main()
     if op == "Editar Perfil": EditarPerfilUI.main()
+  def menu_temporario():
+    op = st.sidebar.selectbox("Menu", ["Editar Perfil","Abrir Conta","Depositar","Sacar","Transferir","Listar Transferencias"])
     if op == "Abrir Conta": AbrirContaUI.main()
     if op == "Listar Contas": ListarContasUI.main()
     if op == "Depositar": DepositarUI.main()
     if op == "Sacar": SacarUI.main()
     if op == "Transferir": TransferirUI.main()
     if op == "Listar Transferencias": ListarTransferenciasUI.main()
+    if op == "Editar Perfil": EditarPerfilUI.main()
+
   
   def btn_logout():
     if st.sidebar.button("Logout"):
@@ -37,12 +43,15 @@ class IndexUI:
 
   def sidebar():
     if "cliente_id" not in st.session_state:
-      IndexUI.menu_temporario()   
+      IndexUI.menu_visitante()   
     else:
       st.sidebar.write("Bem-vindo(a), " + st.session_state["cliente_nome"])
+      if st.session_state["cliente_nome"] == "admin": IndexUI.menu_admin()
+      else: IndexUI.menu_cliente()
       IndexUI.btn_logout()  
 
   def main():
+    view.cliente_admin()
     IndexUI.sidebar()
 
 IndexUI.main()
