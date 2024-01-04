ecab123f5c8aa44eee1396708285db5ff92c893d
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-03 21:51:57 -0300

    Update LoginUI.py

--- Templates/LoginUI.py
+++ Templates/LoginUI.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from View import view
+import time
 class loginUI:
   def main():
     st.header("Login")
@@ -7,4 +9,13 @@ class loginUI:
     email = st.text_input("Informe o e-mail")
     senha = st.text_input("Informe a senha")
     if st.button("Entrar"):
-      pass
+      cliente = view.Cliente_Login(email,senha)
+      if cliente is not None:
+        st.success("Login realizado com sucesso")
+        st.success("Bem-vindo(a)," + cliente.get_Nome())
+        st.session_state["cliente_id"] = cliente.get_id()
+        st.session_state["cliente_nome"] = cliente.get_Nome()
+      else:
+        st.error("Usuario ou senha invalido(s)")
+      time.sleep(2)
+      st.rerun()
