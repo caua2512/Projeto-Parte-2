9912149060831cfa6355420f36b39fcf07b14bc2
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-03 21:48:26 -0300

    Update ManterBancoUI.py

--- Templates/ManterBancoUI.py
+++ Templates/ManterBancoUI.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+from View import view
+import time
 class ManterBancoUI:
   def main():
     st.header("Controlar Bancos")
@@ -9,16 +12,56 @@ class ManterBancoUI:
     with tab4: ManterBancoUI.Excluir()    
 
   def Listar():
-    pass
+    Bancos = view.Banco_Listar()
+    if len(Bancos) == 0:
+      st.write("Nenhum Banco Cadastrado")
+    else:
+      dic = []
+      for obj in Bancos:
+        for Banco in Bancos:
+          id = Banco.get_id()
+          Nome = Banco.get_nome()
+          Endereço = Banco.get_endereco()
+          dic.append(obj[id,Nome,Endereço])
+        
+      df = pd.DataFrame(dic, columns=["id", "Nome", "Endereço"])
+      st.dataframe(df)
   def Inserir():
     nome = st.text_input("Informe o nome")
     endereço = st.text_input("Informe o Endereço")
     if st.button("Inserir"):
-      pass
+      try:
+        view.Banco_Inserir(nome,endereço)
+        st.success("Banco inserido com sucesso")
+        time.sleep(2)
+        st.rerun()
+      except:
+        st.error("Informações invalidas")
   def Atualizar():
-    nome = st.text_input("Informe o nome")
-    endereço = st.text_input("Informe o Endereço")
-    if st.button("Atualizar"):
-      pass
+    bancos = view.Banco_Listar()
+    if len(bancos) == 0:
+      st.write("Nenhum banco cadastrado")
+    else:
+      op = st.selectbox("seleciona banco para atualizar", bancos)
+      nome = st.text_input("Informe o nome")
+      endereço = st.text_input("Informe o Endereço")
+      if st.button("Atualizar"):
+        try:
+          view.Banco_Atualizar(op.get_id(), nome, endereço)
+          st.success("Banco atualizado com sucesso")
+          time.sleep(2)
+          st.rerun()
+        except:
+          st.error("Informações invalidas")
   def Excluir():
-    pass
+    bancos = view.Banco_Listar()
+    if len(bancos) == 0:
+      st.write("Nenhum banco cadastrado")
+    else:
+      op = st.selectbox("Escolher banco a ser excluido", bancos)
+      if st.button("Excluir"):
+          id = op.get_id()
+          view.Banco_Excluir(id)
+          st.success("Banco excluido com sucesso")
+          time.sleep(2)
+          st.rerun()
