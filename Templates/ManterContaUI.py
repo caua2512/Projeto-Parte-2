de95bcb5399892a9ba0822909e110215184f343e
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-03 21:50:07 -0300

    Update ManterContaUI.py

--- Templates/ManterContaUI.py
+++ Templates/ManterContaUI.py
@@ -13,7 +13,7 @@ class ManterContaUI:
     with tab4: ManterContaUI.Excluir()    
 
   def Listar():
-    Contas = view.Cliente_Listar()
+    Contas = view.Contas_Listar()
     if len(Contas) == 0:
       st.write("Nenhuma Conta Cadastrada")
     else:
@@ -25,7 +25,7 @@ class ManterContaUI:
           Data_De_Abertura = Conta.get_Data_De_Abertura()
           Numero_Do_Banco = Conta.Get_Numero_Do_Banco()
           Saldo = Conta.get_saldo()
-          dic.append([id, id_Cliente,Data_De_Abertura,Numero_Do_Banco,Saldo])
+          dic.append(obj[id, id_Cliente,Data_De_Abertura,Numero_Do_Banco,Saldo])
         
       df = pd.DataFrame(dic, columns=["id", "ID do Cliente", "Data de abertura", "Numero do banco", "Saldo"])
       st.dataframe(df)
@@ -41,11 +41,32 @@ class ManterContaUI:
         view.Conta_Inserir(cliente.get_id(), data, NumeroB, Saldo)
         st.sucess("Conta Inserida com sucesso")
         time.sleep(2)
-        st.rerun
+        st.rerun()
   def Atualizar():
-    pass
+    contas = view.Conta_Listar()
+    if len(contas) == 0:
+      st.write("Nenhuma conta cadastrada")
+    else:
+      op = st.selectbox("Escolhar a Conta para atualizar", contas)
+      Data_De_Abertura = st.text_input("Informe a data de Abertura em formato: dd/mm/aaaa HH:MM *", op.get_Data_De_Abertura().strftime('%d/%m/%Y %H:%M'))
+      NumeroB = st.text_input("Informe o numero do banco", op.Get_Numero_Do_Banco())
+      Saldo = st.number_input("Informe o Saldo Inical", op.get_saldo())
+      clientes = view.Cliente_Listar
+      cliente_atual = view.Cliente_Listar_Id((op.get_id_Cliente()))
+      if cliente_atual is not None:
+        cliente = st.selectbox("Selecione o novo cliente", clientes, clientes.index(cliente_atual))
+      else:
+        cliente = st.selectbox("Selecione o novo cliente", clientes)
+      if st.button("Atualizar"):
+        try:
+          data = datetime.datetime.strptime(Data_De_Abertura,"%d/%m/%Y %H:%M")
+          view.Conta_Atualizar(op.get_id(), cliente.get_id(), data, NumeroB, Saldo)
+          time.sleep(2)
+          st.rerun()
+        except:
+          st.error("Informações invalidas")
   def Excluir():
-    contas = view.Cliente_Listar()
+    contas = view.Conta_Listar()
     if len(contas) == 0:
       st.write("Nenhuma Conta Cadastrada")
     else:
@@ -54,4 +75,4 @@ class ManterContaUI:
         view.Conta_Excluir(op.get_id())
         st.sucess("Conta excluida com sucesso")
         time.sleep(2)
-        st.rerun
+        st.rerun()
