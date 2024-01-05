2d242ba5765c6abbb1e4c2e8f4786e70bdc899df
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-05 08:48:24 -0300

    Update Listar_Transferencias_RecentesUI.py

--- Templates/Listar_Transferencias_RecentesUI.py
+++ Templates/Listar_Transferencias_RecentesUI.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
+from View import view
 class ListarTransferenciasUI:
   def main():
     st.header("Controlar Bancos")
     ListarTransferenciasUI.Listar()
   def Listar():
-    pass
+    id_logado = st.session_state["cliente_id"]
+    transferencias = view.Listar_Minhas_Transferencias(id_logado)
+    if len(transferencias) == 0:
+      st.write("Nenhuma Conta cadastrada")
+    else:
+      dic = []
+      for obj in transferencias: dic.append(obj.to_json())
+      df = pd.DataFrame(dic)
+      st.dataframe(df)
