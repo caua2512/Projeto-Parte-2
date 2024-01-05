a138a630f48be426253c4df713a38e1d34709829
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-04 23:26:39 -0300

    Update Listar_Minhas_ContasUI.py

--- Templates/Listar_Minhas_ContasUI.py
+++ Templates/Listar_Minhas_ContasUI.py
@@ -1,7 +1,21 @@
 import streamlit as st
+from View import view
+import pandas as pd
+
 class ListarContasUI:
   def main():
     st.header("Controlar Bancos")
     ListarContasUI.Listar()
   def Listar():
-    pass
+    id_logado = st.session_state["cliente_id"]
+    contas = view.listar_contas_do_cliente(id_logado)
+    if len(contas) == 0:
+      st.write("Nenhuma Conta cadastrada")
+    else:
+      dic = []
+      for obj in contas: dic.append(obj.to_json())
+      df = pd.DataFrame(dic)
+      st.dataframe(df)
+
+
+
