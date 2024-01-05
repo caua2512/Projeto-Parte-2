6aeda08364f5ba5c9b7fe75e1a4cdd974ffa290f
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-04 23:29:53 -0300

    Update SacarUI.py

--- Templates/SacarUI.py
+++ Templates/SacarUI.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from View import view
+import time
 class SacarUI:
   def main():
     st.header("Sacar")
     SacarUI.Sacar()
   def Sacar():
+    contas = view.Conta_Listar()
+    conta = st.selectbox("seleciona Conta para sacar", contas)
     saldo = st.number_input("Saldo do saque")
     if st.button("Sacar"):
-      pass
+      view.sacar(conta.get_id(),saldo)
+      st.success("Saque realizado com sucesso")
+      time.sleep(2)
+      st.rerun()
+      
