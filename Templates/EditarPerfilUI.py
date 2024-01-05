6a085ddf35aa194aa0ffbbbb827191cec48809f1
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-05 11:46:06 -0300

    Update EditarPerfilUI.py

--- Templates/EditarPerfilUI.py
+++ Templates/EditarPerfilUI.py
@@ -11,10 +11,11 @@ class EditarPerfilUI:
     if len(clientes) == 0:
       st.write("Nenhum cliente cadastrado")
     else:
-      if st.session_state["cliente_nome"] == clientes[0].get_Nome():
-        id = st.session_state["cliente_nome"]
+      if st.session_state["cliente_nome"] == "admin":
+        id = st.session_state["cliente_id"]
         bancos = view.Banco_Listar()
         banco = st.selectbox("Selecione o novo Banco", bancos)
+        Nome = "admin"
         Data_de_nascimento = st.text_input("Informe a data de nascimento em formato dd/mm/YYYY")
         email = st.text_input("Informe o e-mail")
         cpf = st.text_input("infrome o cpf")
@@ -22,12 +23,12 @@ class EditarPerfilUI:
         senha = st.text_input("Informe a senha")
         if st.button("Editar"):
           data = datetime.datetime.strptime(Data_de_nascimento, '%d/%m/%Y %H:%M')
-          view.Cliente_Editar_Perfil(id,banco.get_id(), "admin", data, email, cpf, fone, senha)
+          view.Cliente_Editar_Perfil(id,banco.get_id(), Nome, data, email, cpf, fone, senha)
           st.success("Cliente atualizado com sucesso")
           time.sleep(2)
           st.rerun()
       else:
-        id = st.session_state["cliente_nome"]
+        id = st.session_state["cliente_id"]
         bancos = view.Banco_Listar()
         banco = st.selectbox("Selecione o novo Banco", bancos)
         nome = st.text_input("Informe o nome")
