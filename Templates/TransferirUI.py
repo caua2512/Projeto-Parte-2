2e742eabab4941f004144fc10fb0a9dea8563dc3
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-04 23:30:16 -0300

    Update TransferirUI.py

--- Templates/TransferirUI.py
+++ Templates/TransferirUI.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from View import view
+import time
 class TransferirUI:
   def main():
     st.header("Transferencia")
     TransferirUI.transferir()
   def transferir():
+    contas = view.Conta_Listar()
+    conta1 = st.selectbox("seleciona conta da transferencia", contas)
+    conta2 = st.selectbox("seleciona conta para depositar", contas)
     saldo = st.number_input("Saldo Da Transferencia")
     if st.button("Transferir"):
-      pass
+      view.transferir(conta1.get_id(),conta2.get_id(),saldo)
+      st.success("Saque realizado com sucesso")
+      time.sleep(2)
+      st.rerun()
