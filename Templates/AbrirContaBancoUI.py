9371fd03e16868c7b762a3e10b25ea9114236e9f
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-04 23:27:01 -0300

    Update AbrirContaBancoUI.py

--- Templates/AbrirContaBancoUI.py
+++ Templates/AbrirContaBancoUI.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from View import view
+import datetime
+import time
 class AbrirContaUI:
   def main():
     st.header("Abrir Conta")
     AbrirContaUI.Inserir()
   def Inserir():
-    DataDeAbertura = st.date_input("Informe o nome")
+    id = st.session_state["cliente_id"]
+    DataDeAbertura = datetime.datetime.now()
+    NumeroDoBanco = st.text_input("Numero do Banco")
     Saldo = st.number_input("Informe o Saldo atual")
-    if st.button("Inserir"):
-      pass
+    if st.button("Abrir Conta"):
+      view.Conta_Inserir(id, DataDeAbertura, NumeroDoBanco, Saldo)
+      st.success("Banco atualizado com sucesso")
+      time.sleep(2)
+      st.rerun()
