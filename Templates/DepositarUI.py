eff5e5cd747f92b41a5671244073c2b4f23aa4cd
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-04 23:28:56 -0300

    Update DepositarUI.py

--- Templates/DepositarUI.py
+++ Templates/DepositarUI.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from View import view
+import time
 class DepositarUI:
   def main():
     st.header("Depositar")
     DepositarUI.Depositar()
   def Depositar():
-    saldo = st.number_input("Saldo do deposito")
+    contas = view.Conta_Listar()
+    conta = st.selectbox("seleciona Conta para depositar", contas)
+    saldo = st.number_input("Saldo do saque")
     if st.button("Depositar"):
-      pass
+      view.depositar(conta.get_id(),saldo)
+      st.success("Deposito realizado com sucesso")
+      time.sleep(2)
+      st.rerun()
+        
