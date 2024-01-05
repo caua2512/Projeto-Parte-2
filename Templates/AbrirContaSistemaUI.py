f809c7a3f0d3811e75167c827fa2d52f92241222
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-04 23:26:21 -0300

    Update AbrirContaSistemaUI.py

--- Templates/AbrirContaSistemaUI.py
+++ Templates/AbrirContaSistemaUI.py
@@ -10,13 +10,13 @@ class AbrirContaSistemaUI:
     bancos = view.Banco_Listar()
     banco = st.selectbox("selecione o Banco",  bancos)
     nome = st.text_input("Informe o nome")
-    Data_de_nascimento = st.date_input("Informe a data de nascimento")
+    Data_de_nascimento = st.text_input("Informe a data de nascimento")
     email = st.text_input("Informe o e-mail")
     cpf = st.text_input("infrome o cpf")
     fone = st.text_input("Informe o fone")
     senha = st.text_input("Informe a senha")
     if st.button("Cadastra-se"):
-      Data = datetime.datetime.strptime(Data_de_nascimento,"%d/%m/%Y")
+      Data = datetime.datetime.strptime(Data_de_nascimento,"%d/%m/%Y %H:%M")
       try:
         view.Cliente_Inserir(banco.get_id(),nome ,Data, email, cpf, fone, senha)
         st.sucess("Cadastrado com sucesso")
