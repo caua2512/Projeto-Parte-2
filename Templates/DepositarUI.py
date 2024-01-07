e51f18f92d37b4005557ab4bc8f28b8be521b129
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-07 20:25:52 -0300

    Update DepositarUI.py

--- Templates/DepositarUI.py
+++ Templates/DepositarUI.py
@@ -10,8 +10,10 @@ class DepositarUI:
     conta = st.selectbox("seleciona Conta para depositar", contas)
     saldo = st.number_input("Saldo do saque")
     if st.button("Depositar"):
-      view.depositar(conta.get_id(),saldo)
-      st.success("Deposito realizado com sucesso")
-      time.sleep(2)
-      st.rerun()
-        
+      try:
+        view.depositar(conta.get_id(),saldo)
+        st.success("Deposito realizado com sucesso")
+        time.sleep(2)
+        st.rerun()
+      except:
+        st.error("Informações invalidas")
