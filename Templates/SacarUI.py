97ebd41bdbdd1066b9b4d290fc22763fa7fe74a3
caua2512 <130388387+caua2512@users.noreply.github.com>
2024-01-07 20:35:47 -0300

    Update SacarUI.py

--- Templates/SacarUI.py
+++ Templates/SacarUI.py
@@ -6,12 +6,16 @@ class SacarUI:
     st.header("Sacar")
     SacarUI.Sacar()
   def Sacar():
-    contas = view.Conta_Listar()
+    id_logado = st.session_state["cliente_id"]
+    contas = view.listar_contas_do_cliente(id_logado)
     conta = st.selectbox("seleciona Conta para sacar", contas)
     saldo = st.number_input("Saldo do saque")
     if st.button("Sacar"):
-      view.sacar(conta.get_id(),saldo)
-      st.success("Saque realizado com sucesso")
-      time.sleep(2)
-      st.rerun()
+      try:
+        view.sacar(conta.get_id(),saldo)
+        st.success("Saque realizado com sucesso")
+        time.sleep(2)
+        st.rerun()
+      except:
+        st.error("Informações invalidas")
       
